f73b76a6d04e55b0021d8062d460878ed04da253
Amadeo Abqary Dhiyaulhaq <deo531.id@gmail.com>
2025-08-21 06:11:51 +0700

    Update SavingsAccount.java

--- SavingsAccount.java
+++ SavingsAccount.java
@@ -4,20 +4,18 @@ public class SavingsAccount {
     public int jumlahpenarikan;
     public double saldoSetelahPenarikan;
     public double sukubunga;
-    
+
     public void cetak(){
         System.out.println("Nama Customer : " + nama);
         System.out.println("Saldo : Rp " + saldo);
         System.out.println();
     }
-
     public void deposit(int x) {
         saldo += x;
         System.out.println("Deposit : Rp " + x);
         System.out.println("Saldo setelah deposit : Rp " + saldo);
         System.out.println();
     }
-
     public double calcInterest(){
         double bunga =  saldo * sukubunga / 12;
         if (bunga <= 0){
@@ -27,21 +25,20 @@ public class SavingsAccount {
         } System.out.println();
         return bunga;
     }
-
     public void withdraw() {
         System.out.println("Anda Melakukan Penarikan");
         System.out.println("Jumlah Penarikan : Rp "+ jumlahpenarikan);
         if (jumlahpenarikan <= 0){
             System.out.println("Penarikan tidak boleh negatif");
         } else if (jumlahpenarikan >= 150000){
-            System.out.println("Maaf Anda Mencapai Batas Nominal Maksimal Sekali Penarikan");
+            System.out.println("Maaf Anda Mencapai Batas Nominal Maksimal Penarikan");
         }else if (saldo <= jumlahpenarikan) {
             System.out.println("Maaf Saldo anda tidak cukup");
             System.out.println("Saldo Anda Sekarang : Rp " + saldo);
         } else {
             saldoSetelahPenarikan = saldo - jumlahpenarikan;
             saldo = saldoSetelahPenarikan;
-            System.out.println("Selamat anda berhasil melakukan penarikan");
+            System.out.println("Selamat anda berhasil melakukan penarikan yaitu Rp 200000");
             System.out.println("Saldo anda sekarang : Rp" + saldoSetelahPenarikan);
         } System.out.println("----------------------");
     }
